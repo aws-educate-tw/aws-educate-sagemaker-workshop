34b3d0b4e3997113f0fc62d0e27896bc3f8e1095
Rich627 <rich.liu0912@hotmail.com>
2024-08-27 21:45:18 +0800

    feat: add example training code using stablelm-2-zephyr-1_6b with 10 samples

--- scripts/run_qlora.py
+++ scripts/run_qlora.py
@@ -1,13 +1,17 @@
 from dataclasses import dataclass, field
 import os
+import sys
+import subprocess
 from transformers import AutoConfig
-# Upgrade flash attention if necessary
+from typing import Optional
+
+# Upgrade flash attention and install required packages
 try:
-    os.system("pip install flash-attn --no-build-isolation --upgrade")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "flash-attn", "--no-build-isolation", "--upgrade"])
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "--no-cache-dir", "-r", "requirements.txt"])
 except:
     print("flash-attn failed to install")
 
-from typing import Optional
 from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
@@ -84,8 +88,8 @@ def create_peft_model(model, gradient_checkpointing=True, bf16=True):
 
 def training_function(script_args, training_args):
     config = AutoConfig.from_pretrained(script_args.model_id)
-    config.use_flash_attention = False
-    config.use_flash_attention_2 = False
+    config.use_flash_attention = script_args.use_flash_attn
+    config.use_flash_attention_2 = script_args.use_flash_attn
 
     # Load dataset
     dataset = load_from_disk(script_args.dataset_path)
@@ -102,7 +106,8 @@ def training_function(script_args, training_args):
         script_args.model_id,
         device_map="auto",
         quantization_config=bnb_config,
-        config=config
+        config=config,
+        trust_remote_code=script_args.trust_remote_code
     )
 
     # Create PEFT model
@@ -178,7 +183,7 @@ def main():
     training_args.fp16 = True
     set_seed(training_args.seed)
 
-    token = script_args.hf_token if script_args.hf_token else os.getenv("HF_TOKEN", None)
+    token = os.environ.get("HF_TOKEN") or script_args.hf_token
     if token:
         print(f"Logging into the Hugging Face Hub with token {token[:10]}...")
         login(token=token)
