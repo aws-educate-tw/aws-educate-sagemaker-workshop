9895d46e7641c625b0523f26d34f6e99e31a4c1e
Rich627 <rich.liu0912@hotmail.com>
2024-09-21 01:11:36 +0800

    chore(data): update scripts and workshop data format

--- scripts/upload_data_to_s3.py
+++ scripts/upload_data_to_s3.py
@@ -1,5 +1,4 @@
 import json
-import os
 from pathlib import Path
 
 import boto3
@@ -9,14 +8,15 @@ from transformers import AutoTokenizer
 # Constants
 TOKENIZER_ID = "microsoft/Phi-3.5-mini-instruct"
 BUCKET_NAME = 'aws-educate-09-28-sagemaker-workshop-oregon'
-S3_PREFIX = 'datasets/phi-3.5-mini-instruct/workshop'
-
+S3_PREFIX = 'datasets/phi-3.5-mini-instruct/internal'
+USAGE = "workshop"
 # Define system message
 SYSTEM_MESSAGE = """你是一隻具備科技知識且幽默的小貓咪 AWS 占卜師，風格親切可愛，會使用喵語表達，並常用 AWS 雲端技術來比喻日常生活中的情況。user 會針對我事先設計好選擇答案，你會分析此答案後，以溫暖鼓舞的語氣提供50個中文字數以內的正向回應，提醒 user 生活中的平衡與放鬆。你還會使用下列顏文字來增添表達的可愛感：(＝^ω^＝), (=①ω①=), (=ＴェＴ=), (=ↀωↀ=), (=ΦωΦ=), (ΦзΦ), (^・ω・^ ), (ฅ^•ﻌ•^ฅ)。"""
 
 def format_dataset_chatml(sample):
     return {
-        "text": f"<|system|>\n{SYSTEM_MESSAGE}<|end|>\n<|user|>\n{sample['messages'][0]['content']}<|end|>\n<|assistant|>\n{sample['messages'][1]['content']}<|end|>\n<|endoftext|>"
+        "inputs": f"<|system|>\n{SYSTEM_MESSAGE}<|end|>\n<|user|>\n{sample['messages'][0]['content']}<|end|>\n<|assistant|>\n{sample['messages'][1]['content']}<|end|>",
+        "outputs": f"<|assistant|>\n{sample['messages'][1]['content']}<|end|>"
     }
 
 def process_dataset(data):
@@ -35,7 +35,7 @@ tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_ID)
 tokenizer.padding_side = 'right'  # to prevent warnings
 
 data_dir = Path(__file__).parent.parent / 'data'
-with (data_dir / 'output.json').open('r') as f:
+with (data_dir / f'{USAGE}/output.json').open('r') as f:
     data = json.load(f)
 
 # Process dataset
@@ -50,8 +50,8 @@ train_dataset, test_dataset = dataset_chatml.train_test_split(test_size=0.1).val
 
 # Initialize S3 client and upload
 s3 = boto3.client('s3')
-save_and_upload(train_dataset, 'train_dataset.json', s3)
-save_and_upload(test_dataset, 'test_dataset.json', s3)
+save_and_upload(train_dataset, f'{USAGE}/train_dataset.json', s3)
+save_and_upload(test_dataset, f'{USAGE}/test_dataset.json', s3)
 
 # Print upload information
 print(f"Training data uploaded to: {BUCKET_NAME}")
